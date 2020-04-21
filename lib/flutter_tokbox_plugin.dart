a473b183767465935aa89db51f6fcd00d0a800fe
Konstantin Epifanov <epifanov.lab@gmail.com>
2020-04-21 16:16:54 +0300

    .

--- lib/flutter_tokbox_plugin.dart
+++ lib/flutter_tokbox_plugin.dart
@@ -6,8 +6,8 @@ const String PLUGIN_CHANNEL_KEY = "flutter_tokbox_plugin";
 const String PLUGIN_VIEW_CHANNEL_KEY = "$PLUGIN_CHANNEL_KEY.view";
 
 class FlutterTokboxPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_tokbox_plugin');
+  static const MethodChannel _channel
+    = const MethodChannel(PLUGIN_CHANNEL_KEY);
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
